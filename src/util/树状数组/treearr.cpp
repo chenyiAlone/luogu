d41189f71309e0159a6b110b64b8c1832a1a0900
chenyiAlone <1193642781@qq.com>
2019-06-02 15:25:33 +0800

    tree array

---
+++ src/util/树状数组/treearr.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int len;
+vector<int> sum;
+
+
+void add(int id, int val) {
+    for (int i = id; i <= len; i += (i & -i)) {
+        sum[i] += val;
+    }
+}
+
+int get(int id) {
+    int res = 0;
+    for (int i = id; i > 0; i -= (i & -i)) {
+        res += sum[i];
+    }
+    return res;
+}
+
+int main() {
+    // cin >> len;
+    // sum = vector<int>(len + 1, 0);
+    sum = {0, 1, 2, 3, 4, 5, 6};
+    cout << 4 << ' ' << get(3);
+
+
+
+    // vector<int> nums(len, 0);
+    // for (int i = 0, t; i < len; i++) {
+    //     cin >> nums[i];
+    // }
+    // vector<int> v(nums);
+    // sort(v.begin(), v.end());
+    // int diff = v.end() - unique(v.begin(), v.end());
+
+    // for (int i = 0; i < diff; i++) {
+    //     v.pop_back();
+    // }
+    
+    // vector<int> dist(len, 0);
+    // for (int i = 0; i < len; i++) {
+    //     dist[i] = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
+    // }
+    // vector<int> res(size, 0);
+    // for (int i = 0; i < size; i++) {
+    //     res[i] = get(dist[i]);
+    //     add(dist[i] + 1, 1);
+    // }
+    // for (auto x : res)
+    //     cout << x << ' ';
+    return 0;
+}
