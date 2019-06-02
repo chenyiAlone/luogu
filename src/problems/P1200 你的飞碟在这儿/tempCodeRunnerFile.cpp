edaeae41fe78689f2a6c39bc202895aa4f49b9af
chenyiAlone <1193642781@qq.com>
2019-06-02 15:19:50 +0800

    1200

---
+++ src/problems/P1200 你的飞碟在这儿/tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string comet, group;
+    cin >> comet >> group;
+    int c_total = 1, g_total = 1;
+    int len = max(group.size(), comet.size());
+    for (int i = 0, j = 0; i < len; i++) {
+        if (i < group.size())
+            g_total *= group[i] - 'A' + 1;
+        if (i < comet.size())
+            c_total *= comet[i] - 'A' + 1;
+    }
+    cout << (g_total % 47 == c_total % 47 ? "GO" : "STAY") << endl; 
+    return 0;
+}
