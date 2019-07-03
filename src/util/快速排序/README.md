5979574947c73cf0d147c35c126cd0be28cad17a
chenyiAlone <1193642781@qq.com>
2019-07-03 10:11:13 +0800

    随机数快排

--- src/util/快速排序/README.md
+++ src/util/快速排序/README.md
@@ -1,6 +1,6 @@
-### 快速排序
+# 首尾中位数快排
 
-#### 中轴的选择
+## 中轴的选择
 
 1. 选择 `nums[lo]`、 `nums[mid]`、 `nums[hi]` 三个数中的中位数
 2. 三个数排序后，`nums[mid]` 的值就是最后要得到的 `pivot`
@@ -8,7 +8,7 @@
 4. `pivot` 即 `nums[hi - 1]` 的值
 
 
-#### 快排核心
+## 核心思路
 
 1. 通过轴心的选择，已经确定了 `nums[lo]` `nums[hi - 1]` 和 `nums[hi]` 三个的值相对于 `pivot` 的大小关系
 
@@ -35,3 +35,37 @@ void sort(int lo, int hi, vector<int> &nums) {
 
 ```
 
+# 随机数快排
+
+## 中轴的选择
+
+使用 `rand()` 随机数函数生成介于 `l -> r` 之间的下标对应的数组元素的值
+
+```cpp
+int pivot = arr[l + rand() % (l - r + 1)];
+```
+
+## 核心思路
+
+1. 使用随机数生成一个 `pivot`，`L` 和 `R` 两个指针
+2. 这里的判断条件为 `<=`，退出条件有两种情况
+    - `{ arr[R], arr[L] }`，中间没有元素
+    - `{ arr[R], arr[m], arr[L] }`，中间有一个元素
+
+```cpp
+void sort(int l, int r, int arr[]) {
+    if (l >= r) return;
+    int pivot = arr[l + rand() % (r - l + 1)];
+    int L = l, R = r;
+    while (L <= R) {
+        while (L <= R && arr[L] < pivot) L++;
+        while (L <= R && arr[R] > pivot) R--;
+        if (L <= R)  {
+            swap(arr[L], arr[R]);
+            L++;    R--;
+        }
+    }
+    sort(l, R, arr);
+    sort(L, r, arr);
+}
+```
--- src/util/快速排序/quick_sort.cpp
+++ src/util/快速排序/quick_sort.cpp
@@ -1,50 +1,38 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void print_array(vector<int> &nums) {
-    for (int i = 0; i < nums.size(); i++) {
-        cout << nums[i] << ' ';
-    }
+void print_arr(int arr[], int len) {
+    for (int i = 0; i < len; i++)
+        cout << arr[i] << ' ';
     cout << endl;
 }
 
-// 选择 nums[lo]、nums[mid]、nums[hi] 三个数种的中位数作为中轴，将中轴藏到 hi - 1 的位置上
-// nums[lo] <= nums[hi - 1] <= nums[hi]
-int get_pivot(int lo, int hi, vector<int> &nums) {
-    int mid = (lo + hi) >> 1;
-    if (nums[lo] > nums[mid]) swap(nums[lo], nums[mid]);
-    if (nums[mid] > nums[hi]) swap(nums[mid], nums[hi]);
-    if (nums[lo] > nums[mid]) swap(nums[lo], nums[mid]);
-    swap(nums[mid], nums[hi - 1]);
-    cout << "nums[lo]: " << nums[lo] << ", nums[mid]: " << nums[mid] << ", nums[hi]: " << nums[hi] << endl;
-    return nums[hi - 1];
-}
-
-// 使用 ++l 来完成从 lo + 1 到 hi - 2 之间的交换
-void sort(int lo, int hi, vector<int> &nums) {
-    if (lo >= hi) return;
-    int pivot = get_pivot(lo, hi, nums);
-    print_array(nums);
-    int l = lo, r = hi - 1;
-    while (l < r) {
-        while (nums[++l] < pivot);
-        while (nums[--r] > pivot);
-        if (l < r) swap(nums[l], nums[r]);
+void sort(int l, int r, int arr[]) {
+    if (l >= r) return;
+    int base = arr[l + rand() % (r - l + 1)];
+    int L = l, R = r;
+    while (L <= R) {
+        while (L <= R && arr[L] < base) L++;
+        while (L <= R && arr[R] > base) R--;
+        if (L <= R)  {
+            swap(arr[L], arr[R]);
+            L++;    R--;
+        }
     }
-    swap(nums[l], nums[hi - 1]);
-    sort(lo, l - 1, nums);
-    sort(l + 1, hi, nums);
+    sort(l, R, arr);
+    sort(L, r, arr);
 }
 
-void quick_sort(vector<int> &nums) {
-    sort(0, (int)nums.size() - 1, nums);
+void quick_sort(int arr[], int len) {
+    sort(0, len - 1, arr);
 }
+
 
 int main() {
-    vector<int> nums = {4, 2, 4, 5, 1};
-
-    quick_sort(nums);
-    print_array(nums);
+    int arr[] = {3, 2, 1, 5, 0};
+    int len = sizeof(arr) / sizeof(int);
+    cout << len << endl;
+    quick_sort(arr, len);
+    print_arr(arr, len);
     return 0;
 }
